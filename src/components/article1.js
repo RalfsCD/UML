44e0b078dadfa556a39bdb8d1823b227ef79f9e5
Emiiliinjs <168409871+Emiiliinjs@users.noreply.github.com>
2025-03-03 13:33:04 +0200

    Update article1.js

--- src/components/article1.js
+++ src/components/article1.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 function Article1() {
   return (
-     
+     <h1>Kas tā ir un vai tā ir struktūras vai uzvedības modeļa daļa?</h1>
 <a>Use Case diagramma ir uzvedības modeļa daļa, kas attēlo sistēmas funkcionalitāti un aktoru mijiedarbību ar sistēmu.</a>
   );
   }
