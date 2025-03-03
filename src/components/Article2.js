bc55f7986a1432608b63eb3400c7f762c94a6674
Emiiliinjs <168409871+Emiiliinjs@users.noreply.github.com>
2025-03-03 13:11:48 +0200

    Update Article2.js

--- src/components/Article2.js
+++ src/components/Article2.js
@@ -5,7 +5,9 @@ import React from 'react';
 function Article2() {
   return (
      
-<a>atbilde 2</a>
+<a>Definētu sistēmas funkcionalitāti – parāda, ko sistēma dara no lietotāja skatupunkta.
+Identificētu aktorus un to vajadzības – palīdz saprast, kas ir galvenie sistēmas lietotāji.
+Plānotu prasības – tiek izmantota prasību vākšanas procesā, lai izprastu sistēmas uzvedību un funkcijas.</a>
   );
   }
 export default Article2;
