d95a7ad9307bfdff60bc8903a55ecf3bb32b0b4b
Emiiliinjs <168409871+Emiiliinjs@users.noreply.github.com>
2025-03-03 13:12:34 +0200

    Update Article3.js

--- src/components/Article3.js
+++ src/components/Article3.js
@@ -5,7 +5,9 @@ import React from 'react';
 function Article3() {
   return (
      
-<a>atbilde 3</a>
+<a>Aktori – personas vai sistēmas, kas mijiedarbojas ar sistēmu.
+Use Cases – sistēmas funkcionalitāte vai darbības.
+Sistēmas robeža – taisnstūris, kas norāda, kas ir iekļauts sistēmā.</a>
   );
   }
 export default Article3;
